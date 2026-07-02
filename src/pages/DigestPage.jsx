09f134aabf558f9cdd921265a2d3359772fae584
Henry-pixel-dev <henrylaskid5@gmail.com>
2026-07-02 16:16:05 +0100

    update digest page

--- src/pages/DigestPage.jsx
+++ src/pages/DigestPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const DigestPage = () => {
   return (
-    <div>DigestPage</div>
+    <div className="flex h-full w-full flex-col items-center justify-center gap-4 bg-light-bg-primary text-light-text-primary dark:bg-dark-bg-primary dark:text-dark-text-primary">
+      <h1 className='text-2xl font-bold text-light-text-primary dark:text-dark-text-primary'>Coming up soon</h1>
+      <Link to="/dashboard" className="text-light-accent hover:text-light-accent-hover dark:text-dark-accent dark:hover:text-dark-accent-hover">
+        Go back to Dashboard
+      </Link>
+    </div>
   )
 }
 
